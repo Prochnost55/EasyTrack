db831f8b7f09ff42ce8732f5944c1d61140dc14f
Abhishek Singh <aks3555@gmail.com>
2024-12-23 12:23:12 +0530

    added localstorage support in taskService

--- src/app/task/task.service.ts
+++ src/app/task/task.service.ts
@@ -6,12 +6,20 @@ import { type NewTask } from "./task.model";
 export class TaskService {
   private tasks = DUMMY_TASKS;
 
+  constructor() {
+    const tasks = localStorage.getItem('tasks');
+    if(tasks){
+      this.tasks = JSON.parse(tasks);
+    }
+  }
+
   getUserTasks(userId: string) {
     return this.tasks.filter((task) => task.userId === userId);
   }
 
   deleteTask(taskId: string) {
     this.tasks = this.tasks.filter((task) => task.id !== taskId);
+    this.saveTasks();
   }
 
   addTask(task: NewTask, userId: string) {
@@ -22,5 +30,10 @@ export class TaskService {
       summary: task.summary,
       dueDate: task.dueDate,
     });
+    this.saveTasks();
+  }
+
+  saveTasks() {
+    localStorage.setItem('tasks', JSON.stringify(this.tasks));
   }
 }
